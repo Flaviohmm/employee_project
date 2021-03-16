06278baf16625405dc876798ba3362cfd297aae4
Flaviohmm <fhenrique609@gmail.com>
2021-03-16 13:24:53 -0300

    Insert, Update and Delete Employee

--- employee_register/urls.py
+++ employee_register/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.employee_form, name='employee_insert'), # GET and POST req. for insert operation
     path('<int:id>', views.employee_form, name="employee_update"), # GET and POST req. for update operation
+    path('delete/<int:id>/', views.employee_delete, name='employee_delete'),
     path('list/', views.employee_list, name='employee_list') # GET req. to retrieve and display all records.
 ]
